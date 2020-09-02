a78b0a425a7760719192f64830be4b4b4f84c7e9
godofall <antenabr2@gmail.com>
2020-09-02 00:52:55 -0300

    Added the new files to the makelists, and added a horrible way to decide how to transform the events back form void pointers. TODO: FIX THIS SHIT

--- main.cpp
+++ main.cpp
@@ -3,5 +3,17 @@
 
 int main () {
     Controller com;
+    queue<void *> events;
+    while(true){
+        events = com.get_events();
+        while(!events.empty()){
+            Event e = *(Event *) events.front();
+            if(e.get_id() > 16 || e.get_id() < 0){
+                Trigger_Event e = *(Trigger_Event *) events.front();
+            }
+            cout << e.get_id();
+             events.pop();
+        }
+    }
     return 0;
 }
