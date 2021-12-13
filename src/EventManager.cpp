8dd61b7dbe146de73d699526d38d73251b3322df
raphaelabrantes <antenabr2@gmail.com>
2021-12-13 01:57:59 -0300

    EventManager: create c function to start the virtual device

--- src/EventManager.cpp
+++ src/EventManager.cpp
@@ -2,21 +2,52 @@
 // Created by godofall on 08/11/2021.
 //
 #include <EventManager.h>
+#include <fstream>
+#include <linux/uinput.h>
 
 
-namespace EventManager{
+namespace EventManager {
 
-    EventManager::EventManager(Controller::ControllerInputReader &controllerInputReader):
-        _controllerInputReader(controllerInputReader) {
+    EventManager::EventManager(Controller::ControllerInputReader &controllerInputReader) :
+            _controllerInputReader(controllerInputReader) {
+        std::ifstream keysfile("profiles/text.json");
+        if (!keysfile.is_open()) {
+            std::cout << "Failed to open keys" << std::endl;
+            exit(1);
+        }
+        keysfile >> keys;
+        keysfile.close();
+
     }
 
     [[noreturn]] void EventManager::start() {
         auto *event = new js_event;
-        do{
+        do {
             _controllerInputReader.getEvent(event);
-            std::cout << static_cast<int>(event->type) << " " << static_cast<int>(event->number) << " " << static_cast<short>(event->value) << std::endl;
-        }
-        while(true);
+            std::cout << static_cast<int>(event->type) << " " << static_cast<int>(event->number) << " "
+                      << static_cast<int16_t>(event->value) << std::endl;
+
+        } while (true);
 
     }
+}
+
+int start_env(nlohmann::json &js, uinput_setup &setup) {
+    int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+    if (fd < 0) {
+        std::cout << "It was not possible to create the device" << std::endl;
+        exit(1);
+    }
+    ioctl(fd, UI_SET_EVBIT, EV_KEY);
+    for (auto it = js.begin(); it != js.end(); ++it) {
+        ioctl(fd, UI_SET_KEYBIT, static_cast<int>(it.value()));
+    }
+    memset(&setup, 0, sizeof(setup));
+    setup.id.bustype = BUS_USB;
+    setup.id.vendor = 0x1234;
+    setup.id.product = 0x5678;
+    strcpy(setup.name, "Shocker Keyboard");
+    ioctl(fd, UI_DEV_SETUP, &setup);
+    ioctl(fd, UI_DEV_CREATE);
+    return fd;
 }
--- src/include/EventManager.h
+++ src/include/EventManager.h
@@ -4,15 +4,28 @@
 
 #ifndef DUALSHOCKER_EVENTMANAGER_H
 #define DUALSHOCKER_EVENTMANAGER_H
+
 #include <functional>
 #include <ControllerInputReader.h>
+#include <nlohmann/json.hpp>
+#include <linux/uinput.h>
+
+extern "C" {
+int start_env(nlohmann::json &js, uinput_setup &setup);
+}
+
 namespace EventManager {
     class EventManager {
     public:
         EventManager(Controller::ControllerInputReader &controllerInputReader);
+
         [[noreturn]] void start();
+
     private:
         Controller::ControllerInputReader _controllerInputReader;
+        nlohmann::json keys;
+        int pd;
+        uinput_setup uinputSetup;
     };
 }
 
