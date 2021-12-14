e2965463080d3d458dfc180ac34a491f590a6cb7
raphaelabrantes <antenabr2@gmail.com>
2021-12-14 00:17:36 -0300

    JsonMapper + Cmake: Added the JsonMapper class to be responsible for the parser and creation of the keyMapping actions
    Also added to file to the makelist and improved readability

---
+++ src/JsonMapper.cpp
@@ -0,0 +1,41 @@
+//
+// Created by godofall on 13/12/2021.
+//
+#include <fstream>
+#include <iostream>
+#include <JsonMapper.h>
+
+
+std::unordered_map<std::string, Actions::Action *> JsonMapper::createMapping(const std::string &profile) {
+    const nlohmann::json &profileJson = createJsonFromFile(profile);
+    const nlohmann::json &keyMaps = createJsonFromFile("key_codes/keys.json");
+    std::unordered_map<std::string, Actions::Action *> mapping;
+    std::string command("command");
+    //TODO: Startar env
+
+    for (auto it = profileJson.begin(); it != profileJson.end(); ++it) {
+        std::string value(it.value().get<std::string>());
+        if (value.find(command) != std::string::npos) {
+            assert(false);
+        } else {
+            std::cout << " Key: " << it.key() << " Value: " << value << " keymapsAT: " << keyMaps.at(value)
+                      << std::endl;
+            auto *action = new Actions::Button(keyMaps.at(value), 0);
+            mapping[it.key()] = action;
+        }
+    }
+    return mapping;
+}
+
+
+nlohmann::json JsonMapper::createJsonFromFile(const std::string &filePath) {
+    std::ifstream jsonFile(filePath);
+    if (!jsonFile.is_open()) {
+        std::cout << "Failed to open keys" << std::endl;
+        exit(1);
+    }
+    nlohmann::json json;
+    jsonFile >> json;
+    jsonFile.close();
+    return json;
+}
---
+++ src/include/JsonMapper.h
@@ -0,0 +1,20 @@
+//
+// Created by godofall on 13/12/2021.
+//
+
+#ifndef DUALSHOCKER_JSONMAPPER_H
+#define DUALSHOCKER_JSONMAPPER_H
+
+#include <nlohmann/json.hpp>
+#include <unordered_map>
+#include <Action.h>
+
+class JsonMapper {
+public:
+    static std::unordered_map<std::string, Actions::Action *> createMapping(const std::string &profile);
+
+private:
+    static nlohmann::json createJsonFromFile(const std::string &filePath);
+};
+
+#endif //DUALSHOCKER_JSONMAPPER_H
