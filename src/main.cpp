86db2988fbe6def38a729ef88d74b423e6800e1f
raphaelabrantes <antenabr2@gmail.com>
2021-12-15 01:22:34 -0300

    main: now better

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,17 @@
 #include <iostream>
 #include <ControllerInputReader.h>
 #include <EventManager.h>
-int main(int argc, char *argv[]) {
-    std::cout << argc << argv[0] << std::endl;
-    std::string filename("/dev/input/js0");
+#include <JsonMapper.h>
 
-    Controller::ControllerInputReader controllerInputReader(filename);
-    EventManager::EventManager eventManager(controllerInputReader);
-    eventManager.start();
+int main(int argc, char *argv[]) {
+    std::string joystickDeviceFile("/dev/input/js0");
+    std::string profileFile("profiles/text.json");
+    if(argc < 1){
+        profileFile.assign(argv[1]);
+    }
+
+    auto keybinding = JsonMapper::createMapping(profileFile);
+    Controller::ControllerInputReader controllerInputReader(joystickDeviceFile);
+    EventManager::EventManager eventManager(controllerInputReader, keybinding);
+    //eventManager.start();
 }
