986b79b17016a3174821696092f1615a104c83f1
raphaelabrantes <antenabr2@gmail.com>
2022-06-07 16:10:24 -0300

    Main: Fixing the name of variable

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 
 int main(int argc, char *argv[]) {
     std::string joystickDeviceFile("/dev/input/js0");
-    const std::string configPath = getenv("configPath") + std::string("/.shocker/");
+    const std::string configPath = getenv("HOME") + std::string("/.shocker/");
     std::string profileFile(configPath + "profiles/default.json");
     if (argc < 1) {
         profileFile.assign(argv[1]);
